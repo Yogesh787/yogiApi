a35e7582d9aba90e72d478715913542bcab239c6
Yogesh787 <yogeshyadavkthws@gmail.com>
2025-02-04 17:17:40 +0900

    first commit

--- src/mailchimp/mailchimp.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-import { MailchimpService } from './mailchimp.service';
-
-@Controller('mailchimp')
-export class MailchimpController {
-  constructor(private readonly mailchimpService: MailchimpService) {}
-}
